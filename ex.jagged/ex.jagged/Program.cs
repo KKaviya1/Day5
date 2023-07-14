9fd1bd5306d37ae8f73ece5c574505e613e89501
KKaviya1 <139097458+KKaviya1@users.noreply.github.com>
2023-07-14 10:09:08 +0530

    Add files via upload

---
+++ ex.jagged/ex.jagged/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter No of employees: ");
+        int numOfEmployees = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter No of working days for each employee: ");
+        int numOfDays = int.Parse(Console.ReadLine());
+
+        int[][] employeeSalaries = new int[numOfEmployees][];
+
+        for (int i = 0; i < numOfEmployees; i++)
+        {
+            employeeSalaries[i] = new int[numOfDays];
+
+            for (int j = 0; j < numOfDays; j++)
+            {
+                Console.Write($"\nEnter salary for Day {j + 1} of Employee {i + 1}: ");
+                employeeSalaries[i][j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nWeekly Salaries:");
+
+        for (int i = 0; i < numOfEmployees; i++)
+        {
+            int weeklySalary = 0;
+
+            for (int j = 0; j < numOfDays; j++)
+            {
+                weeklySalary += employeeSalaries[i][j];
+            }
+
+            Console.WriteLine($"Employee {i + 1}: {weeklySalary}");
+        }
+
+            Console.ReadKey();
+        }
+    
+}
